9e917814c719d2c7fbc182b43ca0799f48da4d3b
jdesierto <desiertojl98@gmail.com>
2023-11-28 17:34:56 +0800

    Stripe Integration - Payment Processing

--- src/app/services/checkout-service.service.ts
+++ src/app/services/checkout-service.service.ts
@@ -4,6 +4,7 @@ import { Purchase } from '../common/purchase';
 import { Observable } from 'rxjs';
 import { PurchaseResponse } from '../response/purchase-response';
 import { environment } from 'src/environments/environment';
+import { PaymentInfo } from '../common/payment-info';
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +13,16 @@ export class CheckoutServiceService {
   
   private purchaseUrl = environment.desiertoecommerceBackendUrl + '/purchase/buy';
 
+  private paymentIntentUrl = environment.desiertoecommerceBackendUrl + '/api/payment/payment-intent';
+
   constructor(private httpClient: HttpClient) { }
 
 
   purchaseItem(purchase: Purchase): Observable<PurchaseResponse>{
     return this.httpClient.post<PurchaseResponse>(`${this.purchaseUrl}`, purchase);
+  }
+
+  createPaymentIntent(paymentInfo: PaymentInfo): Observable<any>{
+    return this.httpClient.post<PaymentInfo>(this.paymentIntentUrl, paymentInfo);
   }
 }

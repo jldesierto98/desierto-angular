7b4eabdfa85b9269d9155154b3b50f665ef305c6
jdesierto <desiertojl98@gmail.com>
2023-11-30 17:21:51 +0800

    Disable multiple clicks for purchase

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -41,6 +41,8 @@ export class CheckoutComponent implements OnInit {
   cardElement: any;
   displayError: any = "";
 
+  isDisabled: boolean = false;
+
   constructor(private formBuilder: FormBuilder,
               private cartService: AddToCartService,
               private formActionService: FormActionService,
@@ -239,6 +241,8 @@ export class CheckoutComponent implements OnInit {
 
     if(!this.checkoutFormGroup.invalid && this.displayError.textContent === "") {
 
+      this.isDisabled = true;
+
       this.checkOutService.createPaymentIntent(this.paymentInfo).subscribe(
         (paymentIntentResponse) => {
           //send credit card data to stripe.com servers
@@ -263,7 +267,9 @@ export class CheckoutComponent implements OnInit {
               if(result.error){
                 //inform the customer that there was an error
                 alert(`There was an error:  ${result.error.message}`)
+                this.isDisabled = false;
               } else {
+
                 // Call REST API via CheckOutService
                 this.checkOutService.purchaseItem(purchase).subscribe({
                   next: (response: any) => {
@@ -271,9 +277,11 @@ export class CheckoutComponent implements OnInit {
 
                     this.resetCart();
                     this.storage.removeItem('productInCart');
+                    this.isDisabled = false;
                   },
                   error: (err: any) => {
                     alert(`There was an error: ${err.message}`);
+                    this.isDisabled = false;
                   }
                 })
               }
